02d7b6854f39d13d30e528399b3cb2549011a806
pbigalke <paula.bigalke@uni-koeln.de>
2026-01-27 16:59:53 +0000

    polished and tested until line 150 - continue tomorrow

--- constructing_labelled_dataset/construct_labelled_timeseries.py
+++ constructing_labelled_dataset/construct_labelled_timeseries.py
@@ -9,9 +9,9 @@ sys.path.append('..')
 import MWCCH_file_lists_for_studies as mwcch_list
 import chunk_MWCCH_files as mwcch_chunk
 import crop_over_hail_or_overpass as mwcch_crop
-import readers.read_processed_MWCC_H as mwcch_read
+import readers.read_processed_MWCCH as mwcch_read
 import readers.read_MSG as msg_read
-import matching_data.collect_matching_files as match
+import helpers.collect_matching_files as match
 import helpers.datetime_helper as hlp
 
 
@@ -90,82 +90,109 @@ def crop_MSG_timeseries_over_hail_and_save(msg_timeseries, mwcch_data, cropsize,
     # save to given filepath
     msg_timeseries.to_netcdf(filepath)
 
-def folder_from_study_settings(output_path, years, months, area_threshold, msg_res, n_frames, gap, cropsize, min_pix):
-    folder_path = f"{output_path}/{years[0]}-{years[-1]}_{months[0]}-{months[-1]}_areathresh{area_threshold}_" + \
-            f"res{msg_res}min_{n_frames}frames_gap{gap}min_cropsize{cropsize}_min{min_pix}pix"
+def folder_from_study_settings(output_path, years, months, days, area_threshold, msg_res, n_frames, gap, cropsize):
+    """
+    Automatically creates folder path string based on study settings.
+    
+    :param output_path: Path to main output directory.
+    :param years: List of years.
+    :param months: List of months.
+    :param days: List of days.
+    :param area_threshold: Area coverage threshold.
+    :param msg_res: MSG resolution in minutes.
+    :param n_frames: Number of frames.
+    :param gap: Gap in minutes.
+    :param cropsize: Crop size.
+    """
+    folder_path = f"{output_path}/"
+    folder_path += f"{years[0]}-{years[-1]}_" if len(years) > 1 else f"{years[0]}_"
+    folder_path += f"{months[0]}-{months[-1]}_" if len(months) > 1 else f"{months[0]}_"
+    folder_path += f"{days[0]}-{days[-1]}_" if len(days) > 1 else f"{days[0]}_"
+    folder_path += f"areathresh{area_threshold}_res{msg_res}min_{n_frames}frames_gap{gap}min_cropsize{cropsize}"
     return folder_path
 
 # %%
-def construct_labelled_MSG_timeseries(path, years, months, area_threshold, msg_res, n_frames, gap, cropsize, min_pix):
-    # mwcch_path = "/net/merisi/pbigalke/data/MWCC-H/netcdf"
-    mwcch_path = mwcch_read.MWCCH_MSGGRID_PATH
-
-    output_path = folder_from_study_settings(path, years, months, area_threshold, msg_res, n_frames, gap, cropsize, min_pix)
+def construct_labelled_MSG_timeseries_from_MWCCH_chunks(mwcch_bucket, out_path, years, months, days, 
+                                                        area_threshold, msg_res, n_frames, gap, cropsize, min_pix):
+    
+    # ---------------------------------------------------------------- prepare output folder
+    output_path = folder_from_study_settings(out_path, years, months, days, 
+                                             area_threshold, msg_res, n_frames, gap, cropsize)
+    print("output path: ", output_path)
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     # ---------------------------------------------------------------- get all MWCC-H files
-    # load all mwcc-h files in study period
-    mwcch_files = mwcch_list.read_mwcch_files_for_study_settings(mwcch_path, years, months, area_threshold=area_threshold)
+    # load all mwcc-h files in study period, if file list does not exist yet, it will be created first
+    mwcch_files = mwcch_list.read_mwcch_files_for_study_settings(mwcch_bucket, years, months, days, 
+                                                                 area_threshold=area_threshold)
     print(f"total number of MWCC-H files in study period: {len(mwcch_files)}")
 
     # ---------------------------------------------------------------- group files by timeseries
     # chunk files that are within same timeseries
     mwcch_chunks = mwcch_chunk.chunk_files_by_timerange(mwcch_files, n_frames, msg_res, gap=gap)
     print(f"number of timeseries: {len(mwcch_chunks)}")
+    for g, group in enumerate(mwcch_chunks[:5]):
+        print(f"- example timeseries {g}:")
+        for f in group:
+            print(f"------ {f}")
+
 
     # ---------------------------------------------------------------- loop over timeseries groups
     # loop over mwcch chunks
-    for g, group in enumerate(mwcch_chunks):
+    for g, group in enumerate(mwcch_chunks[:1]):
         if g % 1000 == 0:
             print(f"---- processing timeseries {g}/{len(mwcch_chunks)}", flush=True)
 
-        try:
-            # ------------------------------------------------------------ read last MWCC-H
-            # read in mwcch_file of last frame
-            mwcch_last_frame = mwcch_read.read(group[0], variables=["POH", "hail_class"])
-            
-            # ------------------------------------------------------------ get label
-            # set label to maximum hail class within domain
-            max_hail_class = mwcch_read.max_hail_class(mwcch_last_frame.hail_class.values, min_pixel=min_pix)
+        #try:
+        # ------------------------------------------------------------ read last MWCC-H
+        # read in mwcch_file of last frame
+        mwcch_last_frame = mwcch_read.read(group[0], variables=["POH", "hail_class"])
 
-            # define output path for this label
-            path_label = os.path.join(output_path, f"{max_hail_class}_{mwcch_read.convert_hail_class(max_hail_class, to='name')}")
-            if not os.path.exists(path_label):
-                os.makedirs(path_label)
-
-            # ------------------------------------------------------------ create MSG timeseries
-            # read in MSG time series ending in mwcc-h timestamp
-
-            # get end time of overpass
-            mwcch_end = mwcch_last_frame.end_scan
-
-            # get corresponding MSG time series
-            msg_timeseries = collect_MSG_timeseries(mwcch_end, msg_res, n_frames)
-
-            # ------------------------------------------------------------ get crop extent
-            # get center of mass of max hail class area
-            cg_lon, cg_lat, minlon, maxlon, minlat, maxlat = \
-                mwcch_crop.get_crop_extent_over_maxhailarea(mwcch_last_frame, cropsize, min_pixel=min_pix)
-
-            # ------------------------------------------------------------ crop over hail area or overpass
-            # crop dataset over hail area or overpass area
-            msg_timeseries = msg_timeseries.sel(lon=slice(minlon, maxlon), lat=slice(minlat, maxlat))
-            
-            # add global attributes describing the data
-            msg_timeseries = add_attributes(msg_timeseries, cg_lon, cg_lat)
-
-            # ------------------------------------------------------------ save to file
-            # define output filename
-            dt_end = msg_timeseries.end_time
-            filepath = os.path.join(path_label, f"{dt_end}_res{msg_res}min_{n_frames}frames_cropsize{cropsize}.nc")
-
-            # save to given filepath
-            msg_timeseries.to_netcdf(filepath)
+        print(mwcch_last_frame)
+        continue
         
-        except:
-            print(f"Error processing timeseries {g}/{len(mwcch_chunks)}")
-            continue
+        # ------------------------------------------------------------ get label
+        # set label to maximum hail class within domain
+        max_hail_class = mwcch_read.max_hail_class(mwcch_last_frame.hail_class.values, min_pixel=min_pix)
+
+        # define output path for this label
+        path_label = os.path.join(output_path, f"{max_hail_class}_{mwcch_read.convert_hail_class(max_hail_class, to='name')}")
+        if not os.path.exists(path_label):
+            os.makedirs(path_label)
+
+        # ------------------------------------------------------------ create MSG timeseries
+        # read in MSG time series ending in mwcc-h timestamp
+
+        # get end time of overpass
+        mwcch_end = mwcch_last_frame.end_scan
+
+        # get corresponding MSG time series
+        msg_timeseries = collect_MSG_timeseries(mwcch_end, msg_res, n_frames)
+
+        # ------------------------------------------------------------ get crop extent
+        # get center of mass of max hail class area
+        cg_lon, cg_lat, minlon, maxlon, minlat, maxlat = \
+            mwcch_crop.get_crop_extent_over_maxhailarea(mwcch_last_frame, cropsize, min_pixel=min_pix)
+
+        # ------------------------------------------------------------ crop over hail area or overpass
+        # crop dataset over hail area or overpass area
+        msg_timeseries = msg_timeseries.sel(lon=slice(minlon, maxlon), lat=slice(minlat, maxlat))
+        
+        # add global attributes describing the data
+        msg_timeseries = add_attributes(msg_timeseries, cg_lon, cg_lat)
+
+        # ------------------------------------------------------------ save to file
+        # define output filename
+        dt_end = msg_timeseries.end_time
+        filepath = os.path.join(path_label, f"{dt_end}_res{msg_res}min_{n_frames}frames_cropsize{cropsize}.nc")
+
+        # save to given filepath
+        msg_timeseries.to_netcdf(filepath)
+        
+        # except:
+        #     print(f"Error processing timeseries {g}/{len(mwcch_chunks)}")
+        #     continue
 
 
 # %%
@@ -173,30 +200,39 @@ if __name__ == "__main__":
 
     start_script_at = datetime.datetime.now()
 
-    # study settings
-    years = np.arange(2006, 2024, 1)
-    months = np.arange(4, 10, 1)
-    print("study period: ", years, months)
+    # study period
+    years = [2023]  # np.arange(2006, 2024, 1)
+    months = [9]  # np.arange(4, 10, 1)
+    days = [30]  # np.arange(1, 32, 1)
+    print("study period: ", years, months, days)
     
     # MWCC-H filters
-    area_threshold = 30
+    area_threshold = 30  # area coverage threshold in percentage of whole EXPATS domain
     print("area threshold: ", area_threshold)
+    min_pix = 5  # minimum number of pixels in maximum hail class for qualifying as hail class label
 
     # time series settings
-    msg_res = 15
-    n_frames = 4
-    gap = 15
+    msg_res = 15  # MSG resolution in minutes
+    n_frames = 4  # number of frames
+    gap = 15  # Gap between subsequent timeseries in minutes (if negative results in overlapping timeseries)
     print(f"MSG timeseries settings: resolution {msg_res}, n_frames {n_frames}, gap {gap}")
 
     # cropping settings
-    cropsize = 128
-    min_pix = 5
-    print(f"crop settings: cropsize {cropsize}, min_pix {min_pix}")
+    cropsize = 128  # size of square crop in pixels
+    print(f"crop settings: cropsize {cropsize}")
 
-    # construct dataset
-    path = f"/net/merisi/pbigalke/data/labelled_MSG_timeseries"
-    construct_labelled_MSG_timeseries(path, years, months, area_threshold, msg_res, n_frames, gap, cropsize, min_pix)
+    # Output path for labelled dataset
+    out_path = "/data/labelled_datasets" # f"/net/merisi/pbigalke/data/labelled_MSG_timeseries"
+    print("output path: ", out_path)
+
+    # S3 bucket name where MWCC-H files are stored
+    mwcch_bucket = "mwcch-hail-regrid-msg"
+
+    # run construction of labelled MSG timeseries from MWCC-H chunks
+    construct_labelled_MSG_timeseries_from_MWCCH_chunks(mwcch_bucket, out_path, years, months, days, 
+                                                        area_threshold, msg_res, n_frames, gap, cropsize, min_pix)
 
     print("total runtime: ", datetime.datetime.now() - start_script_at)
+
 
 # %%
